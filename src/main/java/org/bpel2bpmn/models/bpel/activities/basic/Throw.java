a6d7a862739221b3b109e4d988e3df0a9fcab2cf
Leon Kielstra <leon.kielstra@nedap.com>
2018-07-26 04:53:57 +0200

    Add implementation for throw element

--- src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Throw.java
+++ src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Throw.java
@@ -2,21 +2,53 @@ package org.bpel2bpmn.models.bpel.activities.basic;
 
 import org.bpel2bpmn.models.bpel.activities.Activity;
 import org.bpel2bpmn.utilities.builders.BPMNBuilder;
+import org.bpel2bpmn.utilities.validation.ValidationResult;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.impl.instance.EndEventImpl;
+import org.camunda.bpm.model.bpmn.impl.instance.ErrorEventDefinitionImpl;
+import org.camunda.bpm.model.bpmn.impl.instance.ErrorImpl;
+import org.camunda.bpm.model.bpmn.instance.EndEvent;
+import org.camunda.bpm.model.bpmn.instance.Error;
+import org.camunda.bpm.model.bpmn.instance.ErrorEventDefinition;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 
 public class Throw extends Activity {
 
-    /* Throw specific attributes */
-    private String faultName;
-    private String faultVariable;
+    public static final String[] ATTRIBUTES = {
+            "faultName",         // mandatory
+            "faultVariable",     // optional
+    };
 
     public Throw() {
         super();
+        attributes.put("faultName", null);
+        attributes.put("faultVariable", null);
     }
 
     @Override
     public FlowNode toBPMN(BPMNBuilder builder, FlowNode from) {
-        return null;
+        BpmnModelInstance modelInstance = builder.getModelInstance();
+
+        Error error = builder.createElement(modelInstance.getDefinitions(), Error.class);
+        error.setName(getFaultName());
+
+        FlowNode endEvent = builder.createElement(EndEvent.class);
+        ErrorEventDefinition errorEventDef = modelInstance.newInstance(ErrorEventDefinition.class);
+        errorEventDef.setError(error);
+        endEvent.addChildElement(errorEventDef);
+
+        return endEvent;
+    }
+
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult("Throw");
+
+        if (attributes.get("faultName") == null) {
+            result.invalidate();
+            result.addMissingAttribute("faultName");
+        }
+
+        return result;
     }
 
     /*
@@ -24,18 +56,10 @@ public class Throw extends Activity {
      */
 
     public String getFaultName() {
-        return faultName;
-    }
-
-    public void setFaultName(String faultName) {
-        this.faultName = faultName;
+        return attributes.get("faultName");
     }
 
     public String getFaultVariable() {
-        return faultVariable;
-    }
-
-    public void setFaultVariable(String faultVariable) {
-        this.faultVariable = faultVariable;
+        return attributes.get("faultVariable");
     }
 }
