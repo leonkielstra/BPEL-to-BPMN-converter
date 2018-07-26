122d3978962b10e0fbdf29676b05260b92eb91c4
Leon Kielstra <leon.kielstra@nedap.com>
2018-07-26 04:53:57 +0200

    Add implementation for throw element

---
+++ src/main/java/org/bpel2bpmn/utilities/parsers/model/activities/basic/ThrowParser.java
@@ -0,0 +1,31 @@
+package org.bpel2bpmn.utilities.parsers.model.activities.basic;
+
+import org.bpel2bpmn.models.bpel.activities.basic.Throw;
+import org.bpel2bpmn.utilities.parsers.model.activities.BPELObjectParser;
+import org.bpel2bpmn.utilities.validation.ValidationResult;
+import org.jdom.Attribute;
+import org.jdom.Element;
+
+public class ThrowParser {
+
+    public static Throw parse(Element element) throws IllegalStateException {
+        Throw throwInstance = BPELObjectParser.parse(element, Throw.class);
+        parseAttributes(throwInstance, element);
+
+        ValidationResult validationResult = throwInstance.validate();
+        if (!validationResult.isValid()) {
+            throw new IllegalStateException(validationResult.getMessage());
+        }
+
+        return throwInstance;
+    }
+
+    private static void parseAttributes(Throw throwInstance, Element element) {
+        for (String attributeName : Throw.ATTRIBUTES) {
+            Attribute attribute = element.getAttribute(attributeName);
+            if (attribute != null) {
+                throwInstance.addAttribute(attributeName, attribute.getValue());
+            }
+        }
+    }
+}
