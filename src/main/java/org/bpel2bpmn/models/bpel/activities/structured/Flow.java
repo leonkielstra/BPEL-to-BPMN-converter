71f7c19da75ddfc8b81482a8c3ab6843417e039e
Leon Kielstra <leon.kielstra@nedap.com>
2019-03-25 21:59:11 +0100

    Add flow parser

--- src/main/java/org/bpel2bpmn/models/bpel/activities/structured/Flow.java
+++ src/main/java/org/bpel2bpmn/models/bpel/activities/structured/Flow.java
@@ -1,6 +1,7 @@
 package org.bpel2bpmn.models.bpel.activities.structured;
 
 import org.bpel2bpmn.models.bpel.activities.Activity;
+import org.bpel2bpmn.models.bpel.generic.Documentation;
 import org.bpel2bpmn.utilities.builders.BPMNBuilder;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 
@@ -8,20 +9,26 @@ import java.util.ArrayList;
 
 public class Flow extends Activity {
 
-    // TODO: Implement Links
+    private ArrayList<String> links;
     private ArrayList<Activity> activities;
 
     public Flow() {
-        super();
+        links = new ArrayList<>();
+        activities = new ArrayList<>();
     }
 
     @Override
     public FlowNode toBPMN(BPMNBuilder builder, FlowNode from) {
-        return null;
+        // TODO: Implement
+        return from;
     }
 
     public void addActivity(Activity activity) {
         this.activities.add(activity);
+    }
+
+    public void addLink(String name) {
+        this.links.add(name);
     }
 
     /*
@@ -30,5 +37,9 @@ public class Flow extends Activity {
 
     public ArrayList<Activity> getActivities() {
         return activities;
+    }
+
+    public ArrayList<String> getLinks() {
+        return links;
     }
 }
