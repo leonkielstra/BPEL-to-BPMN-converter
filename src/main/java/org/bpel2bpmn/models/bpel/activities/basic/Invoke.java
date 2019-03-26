172010b81badf24a9442e503cc01aaa7c51bb911
Leon Kielstra <leon.kielstra@nedap.com>
2019-03-26 23:26:10 +0100

    Write commented algorithm for mapping of Invoke

--- src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Invoke.java
+++ src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Invoke.java
@@ -2,10 +2,18 @@ package org.bpel2bpmn.models.bpel.activities.basic;
 
 import org.bpel2bpmn.models.bpel.activities.Activity;
 import org.bpel2bpmn.utilities.builders.BPMNBuilder;
+import org.bpel2bpmn.utilities.validation.ValidationResult;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 
-// TODO: Implement; Maybe create a super class for all message events?
 public class Invoke extends Activity {
+
+    public static final String[] ATTRIBUTES = {
+            "partnerLink",      // mandatory
+            "operation",        // mandatory
+            "portType",         // optional
+            "inputVariable",    // optional
+            "outputVariable",   // optional
+    };
 
     public Invoke() {
         super();
@@ -13,6 +21,51 @@ public class Invoke extends Activity {
 
     @Override
     public FlowNode toBPMN(BPMNBuilder builder, FlowNode from) {
-        return null;
+        // TODO: Find most enclosed data object for input or create new one in builder
+
+        // TODO: Create send task -- Connect to process with partnerLink
+        // TODO: Connect send task to input data object
+
+        // Map for synchronous invoke
+        if (isSynchronous()) {
+            // TODO: Create receive task -- Connect to process with partnerLink
+            // TODO: Find most enclosed data object for output or create new one in builder
+            // TODO: Connect receive task to output data object
+        }
+
+        // TODO: Implement fault handlers
+
+        return from; // TODO: return actual node
+    }
+
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult("Invoke");
+
+        if (attributes.get("partnerLink") == null) {
+            result.invalidate();
+            result.addMissingAttribute("partnerLink");
+        }
+
+        if (attributes.get("operation") == null) {
+            result.invalidate();
+            result.addMissingAttribute("operation");
+        }
+
+        return result;
+    }
+
+    private boolean isSynchronous() {
+        return (getOutputVariable() != null);
+    }
+
+    /*
+     * Getters & Setters
+     */
+    public String getInputVariable() {
+        return attributes.get("inputVariable");
+    }
+
+    public String getOutputVariable() {
+        return attributes.get("outputVariable");
     }
 }
