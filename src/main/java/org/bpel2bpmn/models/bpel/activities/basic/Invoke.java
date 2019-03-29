00da3923d0a4844ff2228f7b72526f9329eaab0c
Leon Kielstra <leon.kielstra@nedap.com>
2019-03-29 21:44:49 +0100

    Add Invoke mapping (without fault handlers)

--- src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Invoke.java
+++ src/main/java/org/bpel2bpmn/models/bpel/activities/basic/Invoke.java
@@ -3,7 +3,10 @@ package org.bpel2bpmn.models.bpel.activities.basic;
 import org.bpel2bpmn.models.bpel.activities.Activity;
 import org.bpel2bpmn.utilities.builders.BPMNBuilder;
 import org.bpel2bpmn.utilities.validation.ValidationResult;
-import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.impl.instance.SourceRef;
+import org.camunda.bpm.model.bpmn.impl.instance.Target;
+import org.camunda.bpm.model.bpmn.impl.instance.TargetRef;
+import org.camunda.bpm.model.bpmn.instance.*;
 
 public class Invoke extends Activity {
 
@@ -21,21 +24,58 @@ public class Invoke extends Activity {
 
     @Override
     public FlowNode toBPMN(BPMNBuilder builder, FlowNode from) {
-        // TODO: Find most enclosed data object for input or create new one in builder
+        // Create send task
+        SendTask sendTask = builder.createElement(SendTask.class);
+        Operation operation = builder.createElement(Operation.class);
+        operation.setName(attributes.get("operation"));
+        sendTask.setOperation(operation);
+        Property property = builder.createElement(sendTask, Property.class);
 
-        // TODO: Create send task -- Connect to process with partnerLink
-        // TODO: Connect send task to input data object
+        // Connect send task to input data object
+        SourceRef sourceRef;
+        TargetRef targetRef;
+        if (getInputVariable() != null) {
+            DataObjectReference inputReference = builder.findOrCreateDataObject(getInputVariable());
+            DataInputAssociation inputAssociation = builder.createElement(sendTask, DataInputAssociation.class);
+
+            sourceRef = builder.createElement(inputAssociation, SourceRef.class);
+            sourceRef.setTextContent(inputReference.getId());
+            targetRef = builder.createElement(inputAssociation, TargetRef.class);
+            targetRef.setTextContent(property.getId());
+        }
+
+        // Connect send task to process with partnerLink
+        builder.createMessageFlow(sendTask, attributes.get("partnerLink"), false);
+
+        // Temporarily store last created node
+        FlowNode lastNode = sendTask;
 
         // Map for synchronous invoke
         if (isSynchronous()) {
-            // TODO: Create receive task -- Connect to process with partnerLink
-            // TODO: Find most enclosed data object for output or create new one in builder
-            // TODO: Connect receive task to output data object
+            ReceiveTask receiveTask = builder.createElement(ReceiveTask.class);
+            receiveTask.setOperation(operation);
+            property = builder.createElement(receiveTask, Property.class);
+
+            DataObjectReference outputReference = builder.findOrCreateDataObject(getOutputVariable());
+            DataOutputAssociation outputAssociation = builder.createElement(receiveTask, DataOutputAssociation.class);
+
+            sourceRef = builder.createElement(outputAssociation, SourceRef.class);
+            sourceRef.setTextContent(property.getId());
+            targetRef = builder.createElement(outputAssociation, TargetRef.class);
+            targetRef.setTextContent(outputAssociation.getId());
+
+            // Connect receive task to process with partnerLink
+            builder.createMessageFlow(receiveTask, attributes.get("partnerLink"), true);
+
+            // Connect both message tasks
+            builder.createSequenceFlow(sendTask, receiveTask);
+
+            lastNode = receiveTask;
         }
 
         // TODO: Implement fault handlers
 
-        return from; // TODO: return actual node
+        return lastNode;
     }
 
     public ValidationResult validate() {
