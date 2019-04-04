4af6dd8936f13f4ffec2e2955b696cd7c6285ee7
Leon Kielstra <leon.kielstra@nedap.com>
2019-04-04 18:49:07 +0200

    Add mapping for Scope

--- src/main/java/org/bpel2bpmn/models/bpel/activities/structured/Scope.java
+++ src/main/java/org/bpel2bpmn/models/bpel/activities/structured/Scope.java
@@ -1,10 +1,13 @@
 package org.bpel2bpmn.models.bpel.activities.structured;
 
+import org.bpel2bpmn.exceptions.BPELConversionException;
 import org.bpel2bpmn.models.bpel.BPELObject;
 import org.bpel2bpmn.models.bpel.activities.Activity;
 import org.bpel2bpmn.utilities.builders.BPMNBuilder;
 import org.bpel2bpmn.utilities.structures.MappedPair;
+import org.camunda.bpm.model.bpmn.instance.BpmnModelElementInstance;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.SubProcess;
 
 import java.util.ArrayList;
 
@@ -22,7 +25,24 @@ public class Scope extends Activity {
     }
 
     @Override
-    public MappedPair toBPMN(BPMNBuilder builder, FlowNode from) {
-        return null;
+    public MappedPair toBPMN(BPMNBuilder builder, FlowNode from) throws BPELConversionException {
+        SubProcess subProcess = builder.createElement(SubProcess.class);
+        builder.setCurrentScope(subProcess);
+
+        // Map and connect children.
+        FlowNode lastElement = null;
+        for (BPELObject child : children) {
+            MappedPair mapping = child.toBPMN(builder, lastElement);
+            if (!mapping.isEmpty()) {
+                builder.createSequenceFlow(lastElement, mapping.getStartNode());
+                lastElement = mapping.getEndNode();
+            }
+        }
+
+        // Note: Fault handlers aren't mapped for subprocesses
+
+        builder.setCurrentScope((BpmnModelElementInstance) subProcess.getParentElement());
+
+        return new MappedPair(subProcess);
     }
 }
